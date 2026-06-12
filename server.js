6ac63586f0943b228968e18af5ee4839cd756837
TongmengHAI <tongmeng016@gmail.com>
2026-06-12 19:48:30 +0700

    get request by params

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 
+// routes
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+// req = request, res = response
+app.get('/health', (req, res) => {
+    console.log('Get data from form param: '+req.query.name); // form data pass by params
+    res.send('good!');
+});
+
+app.get('/book/:id', (req, res) => {
+    console.log('Get data from route param: '+req.params.id);  // 
+    res.send('Book!')
 });
 
 app.listen(port, () => {
